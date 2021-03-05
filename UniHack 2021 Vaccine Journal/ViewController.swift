5b4236819ceb79696a31b542c204a03a635b4399
Ashwin Gupta <gupta.ashwin08@gmail.com>
2021-03-05 19:01:54 +1100

    Initial Commit

---
+++ UniHack 2021 Vaccine Journal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniHack 2021 Vaccine Journal
+//
+//  Created by Ashwin Gupta on 5/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
