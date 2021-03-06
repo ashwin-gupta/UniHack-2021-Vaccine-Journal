a0315d08bb29457412a114ab44cc4124b9e15bc0
Ashwin Gupta <gupta.ashwin08@gmail.com>
2021-03-06 15:28:20 +1100

    Finished launch screen and added plane graphic

---
+++ UniHack 2021 Vaccine Journal/ViewControllers/LaunchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LaunchViewController.swift
+//  UniHack 2021 Vaccine Journal
+//
+//  Created by Ashwin Gupta on 6/3/21.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
